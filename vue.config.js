4592c056f7f942b8f3d15be271755beb973925e2
gemmamzhang <gemmamzhang@tencent.com>
2021-07-05 15:07:30 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     parallel: false,
     publicPath: process.env.NODE_ENV === "production" ? "/dist" : "/",
-    indexPath: path.resolve(__dirname, 'index.html'),
+    // indexPath: path.resolve(__dirname, 'index.html'),
     chainWebpack: config => {
         config.module.rule('md')
             .test(/\.md/)
